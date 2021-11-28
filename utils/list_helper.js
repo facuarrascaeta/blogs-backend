3848c6f5fed1f927dbaef996957cafd655575c1c
Facundo Arrascaeta <arrascaeta.facu@gmail.com>
2021-11-28 16:47:36 -0300

    Ejercicios 4.3 a 4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,25 @@
+const dummy = (blogs) => {
+  blogs
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  let favorite = blogs[0]
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > favorite.likes) {
+      favorite = blogs[i]
+    }
+  }
+
+  return favorite
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
